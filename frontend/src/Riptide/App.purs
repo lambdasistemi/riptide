e8bcd0467a667e565f60c454632e47d816a25984
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-07-03 22:07:51 +0100

    feat(frontend): build song page
    Tasks: T005-S2

--- frontend/src/Riptide/App.purs
+++ frontend/src/Riptide/App.purs
@@ -4,11 +4,16 @@ module Riptide.App
 
 import Prelude
 
+import Data.Array as Array
+import Data.Int as Int
 import Data.Maybe (Maybe(..))
+import Data.Traversable (traverse)
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
-import Riptide.Model (App, Page(..))
+import Riptide.Action (ControlKey)
+import Riptide.Model (App, CellId, Page(..), Song, SongId, TrackId)
 import Riptide.Reducer as Reducer
+import Riptide.Validation (valid)
 import Riptide.View.Definitions as Definitions
 import Riptide.View.Id (mintId)
 import Riptide.View.Seed (seedApp)
@@ -22,6 +27,24 @@ data Action
   | Hush
   | NewSong
   | NewToolbox
+  | OpenSong SongId
+  | RenameSong SongId String
+  | DuplicateSong SongId
+  | DeleteSong SongId
+  | RenameTrack TrackId String
+  | SetCtrl TrackId ControlKey String
+  | StopTrack TrackId
+  | AddTrack
+  | DeleteTrack TrackId
+  | AddCell TrackId
+  | SelectCell TrackId CellId
+  | ToggleCell TrackId CellId
+  | EditCode TrackId CellId String
+  | DeleteCell TrackId CellId
+  | StartEdit String String
+  | StopEdit
+  | FocusCell CellId
+  | BlurCell CellId
 
 component :: forall query input output m. MonadAff m => H.Component query input output m
 component =
@@ -35,7 +58,7 @@ render :: forall slots m. App -> H.ComponentHTML Action slots m
 render app =
   Shell.render shellActions app
     case app.page of
-      SongPage -> Song.render app
+      SongPage -> Song.render songActions app
       DefsPage -> Definitions.render app
 
 shellActions :: Shell.ShellActions Action
@@ -46,6 +69,29 @@ shellActions =
   , hush: Hush
   , newSong: NewSong
   , newToolbox: NewToolbox
+  }
+
+songActions :: Song.SongActions Action
+songActions =
+  { newSong: NewSong
+  , openSong: OpenSong
+  , renameSong: RenameSong
+  , duplicateSong: DuplicateSong
+  , deleteSong: DeleteSong
+  , renameTrack: RenameTrack
+  , setCtrl: SetCtrl
+  , stopTrack: StopTrack
+  , addTrack: AddTrack
+  , deleteTrack: DeleteTrack
+  , addCell: AddCell
+  , selectCell: SelectCell
+  , toggleCell: ToggleCell
+  , editCode: EditCode
+  , deleteCell: DeleteCell
+  , startEdit: StartEdit
+  , stopEdit: StopEdit
+  , focusCell: FocusCell
+  , blurCell: BlurCell
   }
 
 handleAction :: forall output m. MonadAff m => Action -> H.HalogenM App Action () output m Unit
@@ -70,3 +116,72 @@ handleAction = case _ of
   NewToolbox -> do
     toolboxId <- H.liftEffect (mintId "tb")
     H.modify_ (Reducer.newToolbox toolboxId)
+  OpenSong songId ->
+    H.modify_ (Reducer.openSong songId)
+  RenameSong songId name ->
+    H.modify_ (Reducer.renameSong songId name)
+  DuplicateSong songId -> do
+    app <- H.get
+    case songById songId app of
+      Just source -> do
+        newSongId <- H.liftEffect (mintId "s")
+        trackIds <- traverse (const (H.liftEffect (mintId "t"))) source.tracks
+        cellIds <- traverse (const (H.liftEffect (mintId "c"))) (Array.concatMap _.cells source.tracks)
+        H.modify_ (Reducer.duplicateSong songId { songId: newSongId, trackIds, cellIds })
+      Nothing ->
+        pure unit
+  DeleteSong songId ->
+    H.modify_ (Reducer.deleteSong songId)
+  RenameTrack trackId name ->
+    H.modify_ (Reducer.renameTrack trackId name)
+  SetCtrl trackId key raw ->
+    case Int.fromString raw of
+      Just value -> H.modify_ (Reducer.setCtrl trackId key value)
+      Nothing -> pure unit
+  StopTrack trackId ->
+    H.modify_ (Reducer.stopTrack trackId)
+  AddTrack -> do
+    trackId <- H.liftEffect (mintId "t")
+    H.modify_ (Reducer.addTrack trackId)
+  DeleteTrack trackId ->
+    H.modify_ (Reducer.removeTrack trackId)
+  AddCell trackId -> do
+    cellId <- H.liftEffect (mintId "c")
+    H.modify_ (Reducer.addCell trackId cellId)
+  SelectCell trackId cellId ->
+    H.modify_ (Reducer.selectCell trackId cellId)
+  ToggleCell trackId cellId -> do
+    app <- H.get
+    if app.engine && cellIsLaunchable trackId cellId app then
+      H.modify_ (Reducer.toggleCell trackId cellId)
+    else
+      pure unit
+  EditCode trackId cellId code ->
+    H.modify_ (Reducer.editCode trackId cellId code)
+  DeleteCell trackId cellId ->
+    H.modify_ (Reducer.removeCell trackId cellId)
+  StartEdit kind id ->
+    H.modify_ (Reducer.startEdit kind id)
+  StopEdit ->
+    H.modify_ Reducer.stopEdit
+  FocusCell cellId ->
+    H.modify_ \app -> app { focusCell = Just cellId }
+  BlurCell cellId ->
+    H.modify_ \app ->
+      if app.focusCell == Just cellId then app { focusCell = Nothing } else app
+
+songById :: SongId -> App -> Maybe Song
+songById songId app =
+  Array.find (_.id >>> (_ == songId)) app.songs
+
+cellIsLaunchable :: TrackId -> CellId -> App -> Boolean
+cellIsLaunchable trackId cellId app =
+  case app.currentSongId >>= \songId -> songById songId app of
+    Just song ->
+      case Array.find (_.id >>> (_ == trackId)) song.tracks of
+        Just track ->
+          case Array.find (_.id >>> (_ == cellId)) track.cells of
+            Just cell -> (valid cell.code).valid
+            Nothing -> false
+        Nothing -> false
+    Nothing -> false
